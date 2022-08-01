71770bd319c23f773fad7ae80ff05991a69a3f9d
Kishan Kr Sharma <kishan.sharma@wiseyak.com>
2022-08-01 10:56:12 +0545

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Kishan Kr Sharma on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
